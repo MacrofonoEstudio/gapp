1d47a93b24bbe71a01771fbed3cf88a11fef0e9d
MacrofonoEstudio <info@macrofonoestudio.es>
2015-02-08 20:24:45 +0100

    css

--- www/js/app.js
+++ www/js/app.js
@@ -15,5 +15,22 @@ app.run(function($ionicPlatform, $cordovaFacebook) {
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
-  });  
+  });
+});
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html",
+    controller: 'AppCtrl'
+  });
+
+  
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/sessions');
 });
